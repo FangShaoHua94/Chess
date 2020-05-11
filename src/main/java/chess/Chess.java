a70202b7f805e94296b7c23ab5d56e7394b904cb
FangShaoHua94 <daythehangedman@hotmail.com>
2020-05-11 19:58:26 +0800

    Add base

---
+++ src/main/java/chess/Chess.java
@@ -0,0 +1,11 @@
+package chess;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Chess extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
